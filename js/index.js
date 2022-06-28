d7956ca9af78f76bca4c82ee5d718fdb83d54410
unknown <cejasjoaquin1998@gmail.com>
2022-06-27 23:56:04 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+  sr.reveal('.firstRoll', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px'
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.secondRoll', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+  });window.sr = ScrollReveal();
+  sr.reveal('.thirdRoll', {
+    duration: 5000,
+    origin: 'bottom',
+    distance: '-100px'
+  });window.sr = ScrollReveal();
+  sr.reveal('.fourRoll', {
+    duration: 6000,
+    origin: 'left',
+    distance: '-100px'
+  });window.sr = ScrollReveal();
+  sr.reveal('.fiveRoll', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+  });
